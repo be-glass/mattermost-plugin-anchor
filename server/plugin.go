aabf52af6577737f1ca15e14f986d556d2e3ad4a
be-glass <be.glass@github.com>
2024-10-19 16:30:19 +0200

    debug setup

--- server/plugin.go
+++ server/plugin.go
@@ -2,27 +2,43 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"sync"
-
+	"github.com/mattermost/mattermost/server/public/model"
 	"github.com/mattermost/mattermost/server/public/plugin"
+	"strings"
 )
 
-// Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
-type Plugin struct {
+type HelloPlugin struct {
 	plugin.MattermostPlugin
-
-	// configurationLock synchronizes access to the configuration.
-	configurationLock sync.RWMutex
-
-	// configuration is the active plugin configuration. Consult getConfiguration and
-	// setConfiguration for usage.
-	configuration *configuration
+	configuration *Configuration // Add this field to store the configuration
 }
 
-// ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
-func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+func (p *HelloPlugin) OnActivate() error {
+	// Register the /hello command
+	err := p.API.RegisterCommand(&model.Command{
+		Trigger:          "hello",
+		AutoComplete:     true,
+		AutoCompleteDesc: "Respond with a greeting",
+		AutoCompleteHint: "",
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-// See https://developers.mattermost.com/extend/plugins/server/reference/
+func (p *HelloPlugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
+	command := strings.TrimSpace(args.Command)
+	if command == "/hello" {
+		// Respond with a greeting
+		return &model.CommandResponse{
+			ResponseType: model.CommandResponseTypeInChannel,
+			Text:         fmt.Sprintf("Hello, %s!", args.UserId),
+		}, nil
+	}
+
+	return &model.CommandResponse{
+		ResponseType: model.CommandResponseTypeEphemeral,
+		//Text:         "Unknown command. Please use `/hello`.",
+		Text: args.Command,
+	}, nil
+}
